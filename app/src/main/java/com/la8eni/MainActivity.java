06c871025e6b6fb7a3d3898c41809c659b3f9174
mostafa <mostafamansour163@yahoo.com>
2021-09-04 22:33:11 +0200

    first commit

---
+++ app/src/main/java/com/la8eni/MainActivity.java
@@ -0,0 +1,19 @@
+package com.la8eni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
